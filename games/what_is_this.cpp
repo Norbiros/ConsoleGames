cf70d1248480d763d6050fb112ead83f186583ad
Norbiros <norbiros@protonmail.com>
2022-05-13 15:14:00 +0200

    New working library for json!

--- games/what_is_this.cpp
+++ games/what_is_this.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int whatIsThis() {
  
   //FileReadStream(FILE *fp, char *buffer, std::size_t bufferSize)
-  FILE* fp = fopen("games/a.json", "r"); // non-Windows use "r"
+  FILE* fp = fopen("games/words_and_answers.json", "r"); // non-Windows use "r"
  
   char readBuffer[65536];
   FileReadStream is(fp, readBuffer, sizeof(readBuffer));
@@ -18,6 +18,11 @@ int whatIsThis() {
   d.ParseStream(is);
  
   fclose(fp);
+  
+  if (d.HasParseError()) {
+    cout << "Error with rapidjson:" << endl << d.GetParseError() << endl;
+    return 1;
+  }
 
   cout << d["hello"].GetString() << endl;
   
